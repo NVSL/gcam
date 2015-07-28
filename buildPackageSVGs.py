91f9e9b486a5401cc1f66aa8e9d17cc1b0a8e022
Steven Swanson <swanson@cs.ucsd.edu>
2015-07-28 06:48:53 +0000

    Modularized so we can generate one package at a time.
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/gcam@21713 2cf53259-273a-af40-bda6-69d31023b8ec

--- buildPackageSVGs.py
+++ buildPackageSVGs.py
@@ -15,13 +15,8 @@ import UtilVisitors
 import Dingo.Component
 from io import StringIO
 
-
-def buildPackageSVGs(library,outputDir,gcam,drawOrigins=False):
-    
-    # This step expand each 'element' tag to include a copy of the package from
-    # the library.  Now, we can style/manipulate/etc. each of the instances
-    # independently.
-
+def preprocessLibrary(libname, gcam):
+    library = EagleLibrary(libname)
     # Replace layer numbers with names 
     UtilVisitors.NamedLayers(EagleLayers(library.getLayers())).visit(library.getRoot())  
     # flip everything, if we are rendering the backside of the library.
@@ -42,31 +37,47 @@ def buildPackageSVGs(library,outputDir,gcam,drawOrigins=False):
 
     board = library
     execfile(gcam) # execute the gcam file.
+    return library
+
+def getSVGForPackage(library, drawOrigins, package, filename):
+    r = library.getRoot().find("./drawing/library/packages/package[@name='{}']".format(package))
+    assert r is not None
+    bbox = Dingo.Component.get_bounding_rectangle(r)
+    out = svgwrite.Drawing(filename, size=(str(bbox.width) + "mm", str(bbox.height) + "mm"))
+    out.viewbox(bbox.left(), bbox.top(), bbox.width, bbox.height)
+
+    PSVGVisitor.PSVGVisitor(drawOrigins, out, False, False).go(r)
+    move_group = svgwrite.container.Group()
+    move_group.attribs["class"]= "gtron_package_template";
+    for e in out.elements:
+        move_group.add(e)
+    out.elements = [move_group]
+    svg_data = out.tostring()
+
+    xml_data = ET.parse(StringIO(svg_data))
+    xml_data.getroot().set("eagle_library",library.name)
+    xml_data.getroot().set("eagle_package",r.get("name"))
+    for i in xml_data.getroot().xpath("//svg:g[@class='gtron_package_template']",
+                                      namespaces={'svg': 'http://www.w3.org/2000/svg'}):
+        i.set("{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}insensitive","true")
+
+    ET.SubElement(xml_data.getroot(), "{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}namedview",
+                  {"showborder":"false"})
+    open(filename,"w").write(ET.tostring(xml_data));
+
+
+def buildPackageSVGs(libname,outputDir,gcam,drawOrigins=False):
+    
+    # This step expand each 'element' tag to include a copy of the package from
+    # the library.  Now, we can style/manipulate/etc. each of the instances
+    # independently.
+
+    library = preprocessLibrary(EagleLibrary(libname), gcam);
 
     for r in library.getRoot().findall("./drawing/library/packages/package"):
-        bbox = Dingo.Component.get_bounding_rectangle(r)
         name = os.path.join(outputDir,(r.get("name")+".svg").replace("/",""))
-        out = svgwrite.Drawing(name, size=(str(bbox.width) + "mm", str(bbox.height) + "mm"))
-        out.viewbox(bbox.left(), bbox.top(), bbox.width, bbox.height)
-
-        PSVGVisitor.PSVGVisitor(drawOrigins, out, False, False).go(r)
-        move_group = svgwrite.container.Group()
-        move_group.attribs["class"]= "gtron_package_template";
-        for e in out.elements:
-            move_group.add(e)
-        out.elements = [move_group]
-        svg_data = out.tostring()
-
-        xml_data = ET.parse(StringIO(svg_data))
-        xml_data.getroot().set("eagle_library",library.name)
-        xml_data.getroot().set("eagle_package",r.get("name"))
-        for i in xml_data.getroot().xpath("//svg:g[@class='gtron_package_template']",
-                                          namespaces={'svg': 'http://www.w3.org/2000/svg'}):
-            i.set("{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}insensitive","true")
-
-        ET.SubElement(xml_data.getroot(), "{http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd}namedview",
-                      {"showborder":"false"})
-        xml_data.write(open(name, "w"))
+        data = getSVGForPackage(library, drawOrigins, r.get("name"), name);
+        
 
 if __name__ == "__main__":
 
@@ -76,10 +87,9 @@ if __name__ == "__main__":
     parser.add_argument("--gcam", required=True, type=str, nargs=1, dest='gcamfile', help="gcam file")
     parser.add_argument("--draworigins", required=False, action='store_true', dest='draworigins', help="Draw origins for sub elements")
     args = parser.parse_args()
-    library = EagleLibrary(args.lbrfile[0])
 
 
-    buildPackageSVGs(library=library,
+    buildPackageSVGs(libname=args.lbrfile[0],
                      gcam=args.gcamfile[0],
                      outputDir=args.output[0],
                      drawOrigins=args.draworigins);
