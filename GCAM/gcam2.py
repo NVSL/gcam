5900c757b862e8ce484bbe842a01e4bb1caa29bd
Steven Swanson <swanson@cs.ucsd.edu>
2016-02-17 15:35:02 -0800

    factor out functionality into a function

--- GCAM/gcam2.py
+++ GCAM/gcam2.py
@@ -13,9 +13,36 @@ import sys
 from OverlapCheck import *
 import shapely.geometry as shapes
 
+def brd2svg(brd):
+    whole_board = brd.get_geometry("Dimension", polygonize_wires=SEFP.POLYGONIZE_STRICT)
+    topCopper = brd.get_geometry("Top")
+    tPlace = brd.get_geometry("tPlace")
+    holes =  brd.get_geometry("Holes")
+    tStop = brd.get_geometry(layer_query="tStop")
+
+    board = whole_board.difference(holes)
+    topCopper = topCopper.difference(holes)
+
+    mask = tStop
+    mask = whole_board.difference(tStop)
+#    tPlace = tPlace.difference(tStop)
+        
+    results = [polygon_as_svg(board, style="fill:#fff49a"),
+               polygon_as_svg(topCopper, style="fill:#ffb600"),
+               polygon_as_svg(mask, style="fill:#00ff00; fill-opacity:0.5"),
+               polygon_as_svg(tPlace, style="stroke:#ffffff; stroke-width:0.1mm;fill:none")]
+               
+    svg = """<svg><defs>
+    <link href="my-style.css" type="text/css" rel="stylesheet" 
+    xmlns="http://www.w3.org/1999/xhtml"/>
+    </defs>{}</svg>""".format("".join(results))
+
+    return svg
+    
 def go(argv=None):
     parser = argparse.ArgumentParser(description="Check a design for design rule violations.")
     parser.add_argument("--brd", required=True,  type=str, help="input board")
+    parser.add_argument("--out", required=True,  type=str, help="output svg")
     parser.add_argument("-v", required=False,  action='store_const', const=1, default=0, dest="verbose", help="Be verbose")
     parser.add_argument("-vv", required=False,  action='store_const', const=2, default=0, dest="verbose", help="Be very verbose")
     parser.add_argument("-vvv", required=False,  action='store_const', const=3, default=0, dest="verbose", help="Be very, very, verbose")
@@ -28,30 +55,7 @@ def go(argv=None):
                 
     brd = Swoop.ext.ShapelySwoop.ShapelySwoop.from_file(args.brd)
 
-
-    board = brd.get_geometry("Dimension", polygonize_wires=SEFP.POLYGONIZE_STRICT)
-    topCopper = brd.get_geometry("Top")
-    tPlace = brd.get_geometry("tPlace")
-    holes =  brd.get_geometry("Holes")
-    tStop = brd.get_geometry(layer_query="tStop")
-
-    board = board.difference(holes)
-    topCopper = topCopper.difference(holes)
-    tPlace = tPlace.difference(tStop)
-    
-    mask = board.difference(tStop)
-        
-    results = [polygon_as_svg(board, style="fill:#fff49a"),
-               polygon_as_svg(topCopper, style="fill:#ffb600"),
-               polygon_as_svg(mask, style="fill:#00ff00; fill-opacity:0.5"),
-               polygon_as_svg(tPlace, style="stroke:#ffffff; stroke-width:0.1mm;fill:none")]
-               
-    svg = """<svg><defs>
-    <link href="my-style.css" type="text/css" rel="stylesheet" 
-    xmlns="http://www.w3.org/1999/xhtml"/>
-    </defs>{}</svg>""".format("".join(results))
-    
-    open("out.svg", "w").write(svg)
+    open(args.out, "w").write(brd2svg(brd))
     
 def main():
     r = go(sys.argv[1:])
